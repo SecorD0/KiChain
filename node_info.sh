cd33cc9c494573bda7993b8425e02ad1ac197b34
SecorD0 <vovan6573@gmail.com>
2021-09-03 02:38:21 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -5,7 +5,7 @@ language="EN"
 raw_output=false
 # Data
 node_tcp=$(cat $HOME/kichain/kid/config/config.toml | grep -oPm1 "(?<=^laddr = \")([^%]+)(?=\")")
-status=$(kicli status --node $node_tcp --home $HOME/kichain/kicli/)
+status=$(kid status --node $node_tcp --home $HOME/kichain/kid/)
 node_info=$(kid query staking validators --limit 1500 --output json | jq -r '.validators[] | select(.description.moniker=='\"$kichain_moniker\"')')
 # Variables
 moniker=$(jq ".description.moniker" <<< $node_info | tr -d '"')
